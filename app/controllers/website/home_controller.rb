7ca629315b14a7909e7f21860c56004835c157c5
Cuong Pham <cuongpham@Cuongs-MacBook-Pro.local>
2014-06-11 20:22:29 +0700

    Fix

--- app/controllers/website/home_controller.rb
+++ app/controllers/website/home_controller.rb
@@ -4,21 +4,23 @@ class Website::HomeController < ApplicationController
   def index
     @locations = Location.all
     @posts     = Post.where(show_in_home_page: true).paginate(:page => params[:page], :per_page => 4)
-
+    @title     = 'Trang chủ'
   end
 
   def post
     @locations = Location.all
     @post = Post.find(params[:id])
+    @title     = @post.title
   end
 
   def news
     @posts = Post.all.order("posts.order asc, posts.updated_at desc")
     @locations = Location.all
+    @title     = "Tin tức"
   end
 
   def contact
-
+    @title     = "Liên hệ"
   end
 
   def search
@@ -26,6 +28,7 @@ class Website::HomeController < ApplicationController
     @from_location = @locations.find{|location| location.id == params[:from_location].to_i}
     @to_location = @locations.find{|location| location.id == params[:to_location].to_i}
     @bus_trips = BusTrip.includes(:location_from, :location_to, :tenant, :bus).where(location_from_id: params[:from_location].to_i, location_to_id: params[:to_location].to_i, started_at: params[:search_from_dt].to_date.beginning_of_day..params[:search_from_dt].to_date.end_of_day)
+    @title     = "Chuyến xe từ #{@from_location.name} đến #{@to_location.name}"
   end
 
   def booking
@@ -33,6 +36,7 @@ class Website::HomeController < ApplicationController
     @trip = BusTrip.includes(:location_from, :location_to, :tenant, :bus).find(params[:id])
     @sale_ticket = Ticket.where(bus_trip_id: params[:id]).pluck(:ticket_sale)
     @sale_ticket = @sale_ticket.join(',').split(',') unless @sale_ticket.blank?
+    @title     = "Đặt vé"
   end
 
   def make_booking
