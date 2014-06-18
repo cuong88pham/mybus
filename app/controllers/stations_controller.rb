fd75996eba21024c99655335ae09e4009da18833
Cuong Pham <cuongpham@Cuongs-MacBook-Pro.local>
2014-06-18 21:05:01 +0700

    fix

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,14 +1,23 @@
 class StationsController < InheritedResources::Base
-  assign_params :name, :code, :location_id
+  assign_params :name, :code, :location_id, :tenant_id
   before_filter :prepare_data, only: [:new, :show, :create, :udpate]
   before_action :authenticate_user!
 
   private
   def prepare_data
+    if current_tenant.schema == 'admin'
+      @tenants = Tenant.all
+    else
+      @tenants = nil
+    end
     @locations = Location.all
   end
 
   def collection
-    @stations = Station.includes(:location).all
+    if current_tenant.schema == 'admin'
+      @stations = Station.includes(:location).all
+    else
+      @stations = Station.by_tenant(current_tenant.id).includes(:location)
+    end
   end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -8,5 +8,5 @@ class Station < ActiveRecord::Base
   has_many :bus_trips
   # Scope Get active station
   scope :actived, where(is_valid: true)
-
+  scope :by_tenant, lambda{|tenant_id| where(tenant_id: tenant_id) unless tenant_id.blank?}
 end
