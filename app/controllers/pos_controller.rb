95a8b6b548c4107ba53e7eb1b9559486b0db587a
Cuong Pham <cuongpham@Cuongs-MacBook-Pro.local>
2014-06-19 21:11:56 +0700

    add sale ticket

--- app/controllers/pos_controller.rb
+++ app/controllers/pos_controller.rb
@@ -5,7 +5,7 @@ class PosController < InheritedResources::Base
     @show_result = false
     if params[:from_location].present? && params[:to_location].present?
       @show_result = true
-      @buses = Bus.by_tenant(current_tenant.id).joins(:bus_trips).where("bus_trips.location_from_id = ? AND bus_trips.location_to_id = ? AND bus_trips.started_at between ? AND ?", params[:from_location].to_i, params[:to_location].to_i, params[:search_from_dt].to_date.beginning_of_day, params[:search_from_dt].to_date.end_of_day)
+      @bus_trips = BusTrip.by_tenant(current_tenant.id).includes(:location_from, :location_to, :tenant, :bus).where(location_from_id: params[:from_location].to_i, location_to_id: params[:to_location].to_i, started_at: params[:search_from_dt].to_date.beginning_of_day..params[:search_from_dt].to_date.end_of_day)
 
       @from_location = @locations.find(params[:from_location])
       @to_location = @locations.find(params[:to_location])
